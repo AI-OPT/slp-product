81b0a244ce591c8b070b18e90a6b34b1bb2e2ae2
JackieLiu <liutong6053@163.com>
2016-04-15 14:17:58 +0800

    初始化项目结构

---
+++ product-api/src/main/java/com/ai/slp/product/api/demo/interfaces/IDemoSV.java
@@ -0,0 +1,12 @@
+package com.ai.slp.product.api.demo.interfaces;
+
+/**
+ * demo示例接口,仅用于工程结构说明,正式环境中请删除<br>
+ *
+ * Date: 2016年4月6日 <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * @author liutong5
+ */
+public interface IDemoSV {
+
+}
---
+++ product-api/src/main/java/com/ai/slp/product/api/demo/param/DemoParamsRequest.java
@@ -0,0 +1,14 @@
+package com.ai.slp.product.api.demo.param;
+
+import java.io.Serializable;
+
+/**
+ * 请求参数demo.仅用于工程结构说明,正式环境中请删除<br>
+ *
+ * Date: 2016年4月6日 <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * @author liutong5
+ */
+public class DemoParamsRequest implements Serializable {
+
+}
