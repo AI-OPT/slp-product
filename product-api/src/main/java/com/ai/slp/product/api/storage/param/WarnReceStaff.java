8f63f613658a6cea40c3d271f23bbe9851986137
onle <onle@live.cn>
2016-05-05 20:49:28 +0800

    预警人实现

--- product-api/src/main/java/com/ai/slp/product/api/storage/param/WarnReceStaff.java
+++ product-api/src/main/java/com/ai/slp/product/api/storage/param/WarnReceStaff.java
@@ -9,7 +9,7 @@ import java.util.Date;
  * Copyright (c) 2016 asiainfo.com <br>
  * @author liutong5
  */
-public class WarnReceiveStaff extends WarnReceiveStaffOper {
+public class WarnReceStaff extends WarnReceiveStaffOper {
 
     /**
      * 预警人id
--- product-core/src/main/java/com/ai/slp/product/service/atom/impl/storage/WarnReceiveStaffAtomSVImpl.java
+++ product-core/src/main/java/com/ai/slp/product/service/atom/impl/storage/WarnReceiveStaffAtomSVImpl.java
@@ -1,44 +1,77 @@
 package com.ai.slp.product.service.atom.impl.storage;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.ai.slp.product.constants.CommonSatesConstants;
 import com.ai.slp.product.dao.mapper.bo.storage.WarnReceiveStaff;
 import com.ai.slp.product.dao.mapper.bo.storage.WarnReceiveStaffCriteria;
 import com.ai.slp.product.dao.mapper.interfaces.storage.WarnReceiveStaffMapper;
+import com.ai.slp.product.service.atom.interfaces.ISysSequenceCreditAtomSV;
 import com.ai.slp.product.service.atom.interfaces.storage.IWarnReceiveStaffAtomSV;
+import com.ai.slp.product.util.DateUtils;
 
 public class WarnReceiveStaffAtomSVImpl implements IWarnReceiveStaffAtomSV{
 
     @Autowired
     WarnReceiveStaffMapper warnReceiveStaffMapper;
+    @Autowired
+    ISysSequenceCreditAtomSV sysSequenceCreditAtomSV;
     
     @Override
     public WarnReceiveStaff selectWarnReceiveStaff(String tenantId, String warnReceiveStaffId) {
         WarnReceiveStaffCriteria example = new WarnReceiveStaffCriteria();
-        example.createCriteria();
-        return null;
+        example.createCriteria().andTenantIdEqualTo(tenantId).
+        andWarnReceiveStaffIdEqualTo(warnReceiveStaffId).andStateEqualTo(CommonSatesConstants.STATE_ACTIVE);
+        List<WarnReceiveStaff> warnReceiveStaffList = warnReceiveStaffMapper.selectByExample(example);
+        if(warnReceiveStaffList == null || warnReceiveStaffList.isEmpty())
+            return null;
+        return warnReceiveStaffList.get(0);
     }
 
     @Override
-    public List<WarnReceiveStaff> selectWarnRecList(String tenantId, String storageId) {
-        return null;
+    public List<WarnReceiveStaff> selectWarnRecList(String tenantId, String objectId) {
+        WarnReceiveStaffCriteria example = new WarnReceiveStaffCriteria();
+        example.createCriteria().andTenantIdEqualTo(tenantId)
+        .andObjectIdEqualTo(objectId).andStateEqualTo(CommonSatesConstants.STATE_ACTIVE);
+        return warnReceiveStaffMapper.selectByExample(example);
     }
 
     @Override
     public int insertWarnReceiveStaff(WarnReceiveStaff warnReceiveStaff) {
-        return 0;
+        if(warnReceiveStaff.getOperTime()==null)
+            warnReceiveStaff.setOperTime(DateUtils.currTimeStamp());
+        return warnReceiveStaffMapper.insert(warnReceiveStaff);
     }
 
     @Override
     public int insertWarnReceiveStaff(List<WarnReceiveStaff> warnReceiveStaffList) {
-        return 0;
+        if(warnReceiveStaffList == null || warnReceiveStaffList.isEmpty())
+            return 0;
+        int count = 0;
+        for(WarnReceiveStaff warnReceiveStaff : warnReceiveStaffList){
+            if(warnReceiveStaff.getOperTime()==null)
+                warnReceiveStaff.setOperTime(DateUtils.currTimeStamp());
+            //设置预警类型为库存预警11
+            warnReceiveStaff.setObiectType("11");
+            warnReceiveStaff.setWarnReceiveStaffId(sysSequenceCreditAtomSV.getSeqByName()+"");
+            warnReceiveStaffMapper.insert(warnReceiveStaff);
+            count++;
+        }
+        return count;
     }
 
     @Override
-    public int deleteWarnReceiveStaff(String tenantId, String warnReceiveStaffId) {
-        return 0;
+    public int deleteWarnReceiveStaff(String tenantId, String warnReceiveStaffId, long operId,Timestamp operTime) {
+        WarnReceiveStaff warnReceiveStaff = new WarnReceiveStaff();
+        warnReceiveStaff.setState(CommonSatesConstants.STATE_INACTIVE);
+        warnReceiveStaff.setOperId(operId);
+        warnReceiveStaff.setOperTime(operTime == null ? DateUtils.currTimeStamp() : operTime);
+        WarnReceiveStaffCriteria example = new WarnReceiveStaffCriteria();
+        example.createCriteria().andTenantIdEqualTo(tenantId).andWarnReceiveStaffIdEqualTo(warnReceiveStaffId);
+        return warnReceiveStaffMapper.updateByExampleSelective(warnReceiveStaff, example);
     }
 
 }
