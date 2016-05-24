3656500ad7b7a78f4b137e4666e35c632bb4d884
zhanglh <zhanglh@asiainfo.com>
2016-05-24 13:56:45 +0800

    添加商品查询服务

---
+++ product-api/src/main/java/com/ai/slp/product/api/webfront/param/ProductQueryRequest.java
@@ -0,0 +1,33 @@
+package com.ai.slp.product.api.webfront.param;
+
+import com.ai.opt.base.vo.BaseInfo;
+import com.ai.opt.base.vo.PageInfo;
+
+public class ProductQueryRequest extends BaseInfo {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * sku标识
+     */
+    private String skuId;
+
+    private PageInfo<ProductData> pageInfo;
+
+    public PageInfo<ProductData> getPageInfo() {
+        return pageInfo;
+    }
+
+    public void setPageInfo(PageInfo<ProductData> pageInfo) {
+        this.pageInfo = pageInfo;
+    }
+
+    public String getSkuId() {
+        return skuId;
+    }
+
+    public void setSkuId(String skuId) {
+        this.skuId = skuId;
+    }
+
+}
---
+++ product-core/src/test/java/com/ai/slp/product/service/impl/SearchProductTest.java
@@ -0,0 +1,36 @@
+package com.ai.slp.product.service.impl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ai.opt.base.vo.PageInfo;
+import com.ai.slp.product.api.webfront.interfaces.ISearchProductSV;
+import com.ai.slp.product.api.webfront.param.ProductData;
+import com.ai.slp.product.api.webfront.param.ProductQueryRequest;
+import com.ai.slp.product.api.webfront.param.ProductQueryResponse;
+import com.alibaba.fastjson.JSON;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:context/core-context.xml")
+public class SearchProductTest {
+   
+    @Autowired
+    ISearchProductSV iSearchProductSV;
+
+    @Test
+    public void serachProduct(){
+        ProductQueryRequest request = new ProductQueryRequest();
+        request.setTenantId("432");
+        request.setSkuId("31312");
+        PageInfo<ProductData> pageInfo = new PageInfo<ProductData>();
+        pageInfo.setPageNo(1);
+        pageInfo.setPageSize(10);
+        request.setPageInfo(pageInfo);
+        ProductQueryResponse response = iSearchProductSV.queryProductPage(request);
+        System.out.println("result="+JSON.toJSONString(response.getPageInfo().getResult()));
+
+    }
+}
