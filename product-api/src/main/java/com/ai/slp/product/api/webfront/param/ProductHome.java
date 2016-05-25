cae3eaac18269da1355807495984fcb2d69b20b7
Zh <Zh@10.1.64.66>
2016-05-25 16:55:41 +0800

    增加首页商品服务

---
+++ product-api/src/main/java/com/ai/slp/product/api/webfront/param/ProductHome.java
@@ -0,0 +1,83 @@
+package com.ai.slp.product.api.webfront.param;
+
+public class ProductHome{
+
+    /**
+     * 租户ID
+     */
+    private String tenantId;
+
+    /**
+     * 商品ID
+     */
+    private String prodId;
+
+    /**
+     * 商品名称
+     */
+    private String prodName;
+
+    /**
+     * 商品卖点
+     */
+    private String productSellPoint;
+
+    /**
+     * 销售价
+     */
+    private Long salePrice;
+
+    /**
+     * 商品图
+     */
+    private ProductImage productImage;
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(String prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public String getProductSellPoint() {
+        return productSellPoint;
+    }
+
+    public ProductImage getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(ProductImage productImage) {
+        this.productImage = productImage;
+    }
+
+    public void setProductSellPoint(String productSellPoint) {
+        this.productSellPoint = productSellPoint;
+    }
+
+    public Long getSalePrice() {
+        return salePrice;
+    }
+
+    public void setSalePrice(Long salePrice) {
+        this.salePrice = salePrice;
+    }
+
+}
