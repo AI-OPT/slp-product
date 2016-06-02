f32bc4d003ec86f7159f529097f93db65a8806fa
JackieLiu <liutong6053@163.com>
2016-06-02 16:51:21 +0800

    添加接口编号

--- product-api/src/main/java/com/ai/slp/product/api/webfront/interfaces/IProductHomeSV.java
+++ product-api/src/main/java/com/ai/slp/product/api/webfront/interfaces/IProductHomeSV.java
@@ -1,11 +1,10 @@
 package com.ai.slp.product.api.webfront.interfaces;
 
-import java.util.List;
-
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
-import com.ai.slp.product.api.webfront.param.ProductHomeResponse;
-import com.ai.slp.product.api.webfront.param.ProductHomeRequest;
+import com.ai.slp.product.api.webfront.param.*;
+
+import java.util.List;
 
 public interface IProductHomeSV {
     /**
@@ -15,7 +14,7 @@ public interface IProductHomeSV {
      * @throws BusinessException
      * @throws SystemException
      * @author zhanglh
-     * @ApiCode
+     * @ApiCode PROD_HOME_0100
      */
     public List<ProductHomeResponse> queryHomeDataProduct(ProductHomeRequest request)throws BusinessException, SystemException;
     /**
@@ -24,8 +23,21 @@ public interface IProductHomeSV {
      * @throws BusinessException
      * @throws SystemException
      * @author zhanglh
-     * @ApiCode
+     * @ApiCode PROD_HOME_0101
      */
     public List<ProductHomeResponse> queryHotProduct(ProductHomeRequest request)throws BusinessException, SystemException;
-    
+
+
+    /**
+     * 获取快充产品
+     * @param request
+     * @return
+     * @throws BusinessException
+     * @throws SystemException
+     * @author liutong5
+     * @ApiCode PROD_HOME_0102
+     */
+    public ListWrapperForRes<FastProductInfoRes> queryFastProduct(FastProductReq request)throws BusinessException, SystemException;
+    @interface QueryFastProduct{}
+
 }
