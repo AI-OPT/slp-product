b5642ec65b838db392249dbeb9b6e2e5acde63d1
liutong <liutong6053@163.com>
2016-06-30 16:58:33 +0800

    添加上架时间

---
+++ product-core/src/test/java/com/ai/slp/product/api/webfront/IProductHomeSVTest.java
@@ -0,0 +1,33 @@
+package com.ai.slp.product.api.webfront;
+
+import com.ai.slp.product.api.webfront.interfaces.IProductHomeSV;
+import com.ai.slp.product.api.webfront.param.FastProductInfoRes;
+import com.ai.slp.product.api.webfront.param.FastProductReq;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by jackieliu on 16/6/30.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:context/core-context.xml")
+public class IProductHomeSVTest {
+
+    @Autowired
+    IProductHomeSV productHomeSV;
+
+    @Test
+    public void queryFastProductTest(){
+        FastProductReq fastProductReq = new FastProductReq();
+        fastProductReq.setTenantId("SLP");
+        fastProductReq.setUserType("10");
+        fastProductReq.setProductCatId("10000010020000");
+        fastProductReq.setBasicOrgId("12");
+        fastProductReq.setProvCode(10);
+        FastProductInfoRes infoRes = productHomeSV.queryFastProduct(fastProductReq);
+        System.out.print(infoRes.getLocalMap().size());
+    }
+}
