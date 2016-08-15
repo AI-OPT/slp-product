5f3733854ec88321e53e7ecc41a30e991c8ea12e
liutong <liutong6053@163.com>
2016-08-15 10:23:20 +0800

    添加增加类目单元测试.

--- product-core/src/test/java/com/ai/slp/product/api/productcat/IProductCatSVTest.java
+++ product-core/src/test/java/com/ai/slp/product/api/productcat/IProductCatSVTest.java
@@ -1,15 +1,20 @@
 package com.ai.slp.product.api.productcat;
 
+import com.ai.opt.base.vo.BaseResponse;
 import com.ai.opt.base.vo.PageInfoResponse;
 import com.ai.slp.product.api.productcat.interfaces.IProductCatSV;
 import com.ai.slp.product.api.productcat.param.ProductCatInfo;
 import com.ai.slp.product.api.productcat.param.ProductCatPageQuery;
+import com.ai.slp.product.api.productcat.param.ProductCatParam;
 import com.ai.slp.product.constants.CommonConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jackieliu on 16/8/11.
@@ -32,5 +37,19 @@ public class IProductCatSVTest {
         pageQuery.setProductCatName("");
         PageInfoResponse<ProductCatInfo> catInfoRes = productCatSV.queryPageProductCat(pageQuery);
         System.out.println(catInfoRes.getResponseHeader().getIsSuccess());
+    }
+
+    @Test
+    public void createProductCatTest(){
+        List<ProductCatParam> pcpList = new ArrayList<>();
+        ProductCatParam catParam = new ProductCatParam();
+        catParam.setTenantId(CommonConstants.COMMON_TENANT_ID);
+        catParam.setOperId(1l);
+        catParam.setProductCatName("test12");
+        catParam.setIsChild("Y");
+        catParam.setFirstLetter("A");
+        catParam.setSerialNumber((short)2);
+        pcpList.add(catParam);
+        BaseResponse response = productCatSV.createProductCat(pcpList);
     }
 }
