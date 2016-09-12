6a955b0e51c6bc21ecf8f944a7996ba6a269fe9f
jiaxiaoshuo <jiaxs@asiainfo.com>
2016-09-12 16:50:37 +0800

    新增评论添加校验

--- product-core/src/main/java/com/ai/slp/product/api/productcomment/impl/ProdCommentManagerSVImpl.java
+++ product-core/src/main/java/com/ai/slp/product/api/productcomment/impl/ProdCommentManagerSVImpl.java
@@ -1,5 +1,8 @@
 package com.ai.slp.product.api.productcomment.impl;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +14,7 @@ import com.ai.slp.product.api.productcomment.interfaces.IProdCommentManagerSV;
 import com.ai.slp.product.api.productcomment.param.ProdCommentCreateRequest;
 import com.ai.slp.product.api.productcomment.param.ProdCommentPageRequest;
 import com.ai.slp.product.api.productcomment.param.ProdCommentPageResponse;
+import com.ai.slp.product.api.productcomment.param.ProdCommentVO;
 import com.ai.slp.product.service.business.interfaces.comment.IProdCommentBusiSV;
 import com.ai.slp.product.util.CommonUtils;
 import com.alibaba.dubbo.config.annotation.Service;
@@ -31,7 +35,53 @@ public class ProdCommentManagerSVImpl implements IProdCommentManagerSV {
 	@Override
 	public BaseResponse createProdComment(ProdCommentCreateRequest prodCommentCreateRequest) throws BusinessException, SystemException {
 		CommonUtils.checkTenantId(prodCommentCreateRequest.getTenantId());
+		checkCreateCommentParams(prodCommentCreateRequest);
 		return prodCommentBusiSV.createProdComment(prodCommentCreateRequest);
+	}
+
+	/**
+	 * 检查新增评论入参
+	 * @param prodCommentCreateRequest
+	 * @author jiaxs
+	 * @ApiDocMethod
+	 * @ApiCode
+	 * @RestRelativeURL
+	 */
+	private void checkCreateCommentParams(ProdCommentCreateRequest prodCommentCreateRequest) {
+		List<ProdCommentVO> commentList = prodCommentCreateRequest.getCommentList();
+		if(commentList == null || commentList.size() == 0){
+			throw new BusinessException("commentList 不能为空");
+		}
+		for(ProdCommentVO prodCommentVO : commentList){
+			String commentBody = prodCommentVO.getCommentBody();
+			if(StringUtils.isBlank(commentBody)){
+				throw new BusinessException("commentBody 不能为空");
+			}
+			Long shopScoreFw = prodCommentVO.getShopScoreFw();
+			if(shopScoreFw == null){
+				throw new BusinessException("shopScoreFw 不能为空");
+			}
+			Long shopScoreMs = prodCommentVO.getShopScoreMs();
+			if(shopScoreMs == null){
+				throw new BusinessException("shopScoreMs 不能为空");
+			}
+			Long shopScoreWl = prodCommentVO.getShopScoreWl();
+			if(shopScoreWl == null){
+				throw new BusinessException("shopScoreWl 不能为空");
+			}
+			String skuId = prodCommentVO.getSkuId();
+			if(StringUtils.isBlank(skuId)){
+				throw new BusinessException("skuId 不能为空");
+			}
+			String subOrderId = prodCommentVO.getSubOrderId();
+			if(StringUtils.isBlank(subOrderId)){
+				throw new BusinessException("subOrderId 不能为空");
+			}
+			String userId = prodCommentVO.getUserId();
+			if(StringUtils.isBlank(userId)){
+				throw new BusinessException("userId 不能为空");
+			}
+		}
 	}
 
 }
