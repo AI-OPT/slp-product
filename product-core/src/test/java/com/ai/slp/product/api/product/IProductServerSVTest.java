73c5f2218608022e8cc57ee50ea0a8bd6fbbac02
jiawen <wen6485@139.com>
2016-12-13 11:12:27 +0800

    更新测试类

--- product-core/src/test/java/com/ai/slp/product/api/product/IProductServerSVTest.java
+++ product-core/src/test/java/com/ai/slp/product/api/product/IProductServerSVTest.java
@@ -1,8 +1,11 @@
 package com.ai.slp.product.api.product;
 
 import com.ai.slp.product.api.product.interfaces.IProductServerSV;
+import com.ai.slp.product.api.product.param.ProductSkuInfo;
 import com.ai.slp.product.api.product.param.SkuInfoQuery;
 import com.ai.slp.product.constants.CommonTestConstants;
+import com.google.gson.Gson;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +26,19 @@ public class IProductServerSVTest {
         SkuInfoQuery skuInfoQuery = new SkuInfoQuery();
         skuInfoQuery.setTenantId(CommonTestConstants.COMMON_TENANT_ID);
         skuInfoQuery.setSkuId("1000000000002405");
-        productServerSV.queryProductSkuById(skuInfoQuery);
+        ProductSkuInfo skuById = productServerSV.queryProductSkuById(skuInfoQuery);
+        
+        Gson gson = new Gson();
+		System.out.println(gson.toJson(skuById));
+    }
+    @Test
+    public void queryProductSkuById4ShopCartTest(){
+    	SkuInfoQuery skuInfoQuery = new SkuInfoQuery();
+    	skuInfoQuery.setTenantId(CommonTestConstants.COMMON_TENANT_ID);
+    	skuInfoQuery.setSkuId("1000000000002405");
+    	ProductSkuInfo skuById = productServerSV.queryProductSkuById4ShopCart(skuInfoQuery);
+    	
+    	Gson gson = new Gson();
+    	System.out.println(gson.toJson(skuById));
     }
 }
