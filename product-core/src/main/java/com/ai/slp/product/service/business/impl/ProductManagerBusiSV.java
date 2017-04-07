4ddb030587e376c4dea0c1a3bc3b6e6837aef6b0
jiawen <wen6485@139.com>
2017-04-07 19:03:26 +0800

    完善

--- product-core/src/main/java/com/ai/slp/product/service/business/impl/ProductManagerBusiSV.java
+++ product-core/src/main/java/com/ai/slp/product/service/business/impl/ProductManagerBusiSV.java
@@ -281,7 +281,6 @@ public class ProductManagerBusiSV implements IProductManagerBusiSV {
                 	SESClientFactory.getSearchClient(SearchConstants.SearchNameSpace).bulkInsert(skuInfoList);
                 }
             	
-            	
                 continue;
             }
 
@@ -289,11 +288,47 @@ public class ProductManagerBusiSV implements IProductManagerBusiSV {
             //若为立即上架或预售,则进行上架处理
             if (ProductConstants.Product.UpShelfType.NOW.equals(product.getUpshelfType())
                     || ProductConstants.Product.UpShelfType.PRE_SALE.equals(product.getUpshelfType())){
+            	
                 productBusiSV.changeToInSaleFromAudit(product,productCheckParam.getOperId());
+                
                 //将商品添加至搜索引擎
                 if(ProductConstants.Product.State.IN_SALE.equals(product.getState())){
                 	skuIndexManage.updateSKUIndex(prodId,product.getUpTime().getTime());
                 }
+               
+                
+                //查询es
+            	List<SearchCriteria> searchCriterias = new ArrayList<SearchCriteria>();
+            	searchCriterias.add(new SearchCriteria("productid",
+            			prodId,
+            			new SearchOption(SearchOption.SearchLogic.must, SearchOption.SearchType.querystring)));
+            	
+            	int startSize = 1;
+        		int maxSize = 1;
+        		// 最大条数设置
+        		int pageNo = 1;
+        		int size = 20;
+        		if (pageNo == 1) {
+        			startSize = 0;
+        		} else {
+        			startSize = (pageNo - 1) * size;
+        		}
+        		maxSize = size;
+        		IProductSearch productSearch = new ProductSearchImpl();
+            	Result<SKUInfo> result = productSearch.searchByCriteria(searchCriterias, startSize, maxSize, null);
+            	if (CollectionUtil.isEmpty(result.getContents())) {
+            		logger.error("查询商品失败");
+            		throw new BusinessException("查询es中的商品信息失败");
+        		}
+            	SKUInfo skuInfo = result.getContents().get(0);
+                //更新es
+            	 List<SKUInfo> skuInfoList = new ArrayList<>();
+            	skuInfo.setState(ProductConstants.Product.State.IN_STORE);
+            	if (!CollectionUtil.isEmpty(skuInfoList)){
+                	SESClientFactory.getSearchClient(SearchConstants.SearchNameSpace).bulkInsert(skuInfoList);
+                }
+                
+                
             }else {
                 product.setState(ProductConstants.Product.State.IN_STORE);
                 product.setOperId(productCheckParam.getOperId());
