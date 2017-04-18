c34c56fd0a8f79764b97742bedc523618b52693b
jiawen <wen6485@139.com>
2017-04-18 19:45:48 +0800

    完善代码

--- product-core/src/main/java/com/ai/slp/product/service/business/impl/search/SKUIndexBusiSVImpl.java
+++ product-core/src/main/java/com/ai/slp/product/service/business/impl/search/SKUIndexBusiSVImpl.java
@@ -212,6 +212,7 @@ public class SKUIndexBusiSVImpl implements ISKUIndexBusiSV {
             skuInfo.setProducttype(standedProduct.getProductType());
             //標準品创建时间
             skuInfo.setCreatetime(standedProduct.getCreateTime().getTime());
+            skuInfo.setOperid(selectByProductId.getOperId().toString());
             skuInfo.setSupplierid(standedProduct.getSupplierId());
             // 受众
             //skuInfo.setAudiences(fillSKUAudiences(prodSkuInfo.getTenantid(),prodSkuInfo.getProductid()));
