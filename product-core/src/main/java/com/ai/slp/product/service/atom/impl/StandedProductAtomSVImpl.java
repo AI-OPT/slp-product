84553b2fdc9e643c67b6ac7ba6705dca224355ba
jiawen <wen6485@139.com>
2017-04-26 16:24:04 +0800

    完善更新市场价服务

--- product-core/src/main/java/com/ai/slp/product/service/atom/impl/StandedProductAtomSVImpl.java
+++ product-core/src/main/java/com/ai/slp/product/service/atom/impl/StandedProductAtomSVImpl.java
@@ -184,7 +184,7 @@ public class StandedProductAtomSVImpl implements IStandedProductAtomSV {
 	@Override
 	public int updateMarketPrice(String tenantId, String standedProductId, long marketPrice, Long operId) {
 		StandedProductCriteria example = new StandedProductCriteria();
-		example.createCriteria().andTenantIdEqualTo(tenantId).andStandedProdIdEqualTo(standedProductId);
+		example.createCriteria().andStandedProdIdEqualTo(standedProductId);
 		StandedProduct standedProduct = new StandedProduct();
 		standedProduct.setMarketPrice(marketPrice);
 		standedProduct.setOperId(operId);
